36c867540529caa983665c20a89ca4da31195714
Lisa Leggett <lisa.leggett@bas.ac.uk>
2026-04-23 13:33:23 +0100

    Added further test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -64,16 +64,32 @@ def test_daily_mean_integers():
             [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
             None,
         ),
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [4, 5, 6],
+            None,
+            ValueError,
+        ),
     ])
 
 def test_patient_normalise(test, expected, expect_raises):
-    """Test normalisation works for arrays of one and positive integers.
-       Test with a relative and absolute tolerance of 0.01."""
-
+    """Test normalisation works for arrays of one and positive integers."""
+    if isinstance(test, list):
+        test = np.array(test)
     if expect_raises is not None:
         with pytest.raises(expect_raises):
-            patient_normalise(np.array(test))
+          patient_normalise(test)
 
     else:
-        result = patient_normalise(np.array(test))
+        result = patient_normalise(test)
         npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+
